8aed80b054ee6943ee0d6b63968acccf2bca096a
pkmitl205 <58030205@kmitl.ac.th>
2017-11-08 14:12:02 +0700

    แบบฝึกหัด โปรแกรมหาค่าเกรดเฉลี่ย (GPA)

--- Lab07/Lab07/Program.cs
+++ Lab07/Lab07/Program.cs
@@ -1,18 +1,81 @@
 ﻿using System;
+using System.Collections;
 
 namespace ConsoleAppArray
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            int[] data = { 51, 58, 14, 20, -5, 61, 7, 21, 6 };
-            Console.WriteLine("Length of data = {0}", data.Length);
-            for (int i = 0; i < data.Length; i++)
+
+            ArrayList al = new ArrayList();
+
+            Console.WriteLine("");
+            Console.WriteLine("===============|| Please Enter GPS ||================");
+            for (int i = 0; i <= 7; i++)
             {
-                Console.WriteLine(data[i]);
+                Console.Write("GPS Semester {0} : ", i + 1);
+
+                try
+                {
+                    double val1 = Convert.ToDouble(Console.ReadLine());
+
+                    try
+                    {
+                        if (val1 > 0.0 && val1 <= 4.0)
+                            val1 = al.Add(val1);
+                        else
+                            throw (new Exception("Error!!!! invalid GPA"));
+                    }
+
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
             }
-            Console.ReadLine();
+
+            string str = " ";
+            foreach (double item in al)
+            {
+                str = str + item + Environment.NewLine;
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("==================|| Show GPS ||===================");
+            for (int i = 0; i <= 7; i++)
+            {
+                try
+                {
+                    Console.WriteLine("GPS : Semester {0} = {1}", i + 1, al[i]);
+                }
+                catch (ArgumentException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("==================|| Show GPA ||==================");
+            double sum = 0;
+            for (int i = 0; i < 7; i++)
+                sum = sum + Convert.ToDouble(al[i]);
+            try
+            {
+                Console.WriteLine("GPA : {0}", sum / 8);
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
         }
     }
 }
