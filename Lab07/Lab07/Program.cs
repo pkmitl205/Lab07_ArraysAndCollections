f3061aecbdb23c28584740718b6dcafa55b08f11
pkmitl205 <58030205@kmitl.ac.th>
2017-11-13 13:05:33 +0700

    การคัดลอกข้อมูลในอาร์เรย์

--- Lab07/Lab07/Program.cs
+++ Lab07/Lab07/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 
 namespace ConsoleAppArray
 {
@@ -7,262 +6,45 @@ namespace ConsoleAppArray
     {
         static void Main(string[] args)
         {
-            string[] array1 = new string[]
-      {
-            "Afghanistan",
-            "Albania",
-            "Algeria",
-            "American Samoa",
-            "Andorra",
-            "Angola",
-            "Anguilla",
-            "Antarctica",
-            "Antigua and Barbuda",
-            "Argentina",
-            "Armenia",
-            "Aruba",
-            "Australia",
-            "Austria",
-            "Azerbaijan",
-            "Bahamas",
-            "Bahrain",
-            "Bangladesh",
-            "Barbados",
-            "Belarus",
-            "Belgium",
-            "Belize",
-            "Benin",
-            "Bermuda",
-            "Bhutan",
-            "Bolivia",
-            "Bosnia and Herzegovina",
-            "Botswana",
-            "Bouvet Island",
-            "Brazil",
-            "British Indian Ocean Territory",
-            "Brunei Darussalam",
-            "Bulgaria",
-            "Burkina Faso",
-            "Burundi",
-            "Cambodia",
-            "Cameroon",
-            "Canada",
-            "Cape Verde",
-            "Cayman Islands",
-            "Central African Republic",
-            "Chad",
-            "Chile",
-            "China",
-            "Christmas Island",
-            "Cocos (Keeling) Islands",
-            "Colombia",
-            "Comoros",
-            "Congo",
-            "Congo, the Democratic Republic of the",
-            "Cook Islands",
-            "Costa Rica",
-            "Cote D'Ivoire",
-            "Croatia",
-            "Cuba",
-            "Cyprus",
-            "Czech Republic",
-            "Denmark",
-            "Djibouti",
-            "Dominica",
-            "Dominican Republic",
-            "Ecuador",
-            "Egypt",
-            "El Salvador",
-            "Equatorial Guinea",
-            "Eritrea",
-            "Estonia",
-            "Ethiopia",
-            "Falkland Islands (Malvinas)",
-            "Faroe Islands",
-            "Fiji",
-            "Finland",
-            "France",
-            "French Guiana",
-            "French Polynesia",
-            "French Southern Territories",
-            "Gabon",
-            "Gambia",
-            "Georgia",
-            "Germany",
-            "Ghana",
-            "Gibraltar",
-            "Greece",
-            "Greenland",
-            "Grenada",
-            "Guadeloupe",
-            "Guam",
-            "Guatemala",
-            "Guinea",
-            "Guinea-Bissau",
-            "Guyana",
-            "Haiti",
-            "Heard Island and Mcdonald Islands",
-            "Holy See (Vatican City State)",
-            "Honduras",
-            "Hong Kong",
-            "Hungary",
-            "Iceland",
-            "India",
-            "Indonesia",
-            "Iran, Islamic Republic of",
-            "Iraq",
-            "Ireland",
-            "Israel",
-            "Italy",
-            "Jamaica",
-            "Japan",
-            "Jordan",
-            "Kazakhstan",
-            "Kenya",
-            "Kiribati",
-            "Korea, Democratic People's Republic of",
-            "Korea, Republic of",
-            "Kuwait",
-            "Kyrgyzstan",
-            "Lao People's Democratic Republic",
-            "Latvia",
-            "Lebanon",
-            "Lesotho",
-            "Liberia",
-            "Libyan Arab Jamahiriya",
-            "Liechtenstein",
-            "Lithuania",
-            "Luxembourg",
-            "Macao",
-            "Macedonia, the Former Yugoslav Republic of",
-            "Madagascar",
-            "Malawi",
-            "Malaysia",
-            "Maldives",
-            "Mali",
-            "Malta",
-            "Marshall Islands",
-            "Martinique",
-            "Mauritania",
-            "Mauritius",
-            "Mayotte",
-            "Mexico",
-            "Micronesia, Federated States of",
-            "Moldova, Republic of",
-            "Monaco",
-            "Mongolia",
-            "Montserrat",
-            "Morocco",
-            "Mozambique",
-            "Myanmar",
-            "Namibia",
-            "Nauru",
-            "Nepal",
-            "Netherlands",
-            "Netherlands Antilles",
-            "New Caledonia",
-            "New Zealand",
-            "Nicaragua",
-            "Niger",
-            "Nigeria",
-            "Niue",
-            "Norfolk Island",
-            "Northern Mariana Islands",
-            "Norway",
-            "Oman",
-            "Pakistan",
-            "Palau",
-            "Palestinian Territory, Occupied",
-            "Panama",
-            "Papua New Guinea",
-            "Paraguay",
-            "Peru",
-            "Philippines",
-            "Pitcairn",
-            "Poland",
-            "Portugal",
-            "Puerto Rico",
-            "Qatar",
-            "Reunion",
-            "Romania",
-            "Russian Federation",
-            "Rwanda",
-            "Saint Helena",
-            "Saint Kitts and Nevis",
-            "Saint Lucia",
-            "Saint Pierre and Miquelon",
-            "Saint Vincent and the Grenadines",
-            "Samoa",
-            "San Marino",
-            "Sao Tome and Principe",
-            "Saudi Arabia",
-            "Senegal",
-            "Serbia and Montenegro",
-            "Seychelles",
-            "Sierra Leone",
-            "Singapore",
-            "Slovakia",
-            "Slovenia",
-            "Solomon Islands",
-            "Somalia",
-            "South Africa",
-            "South Georgia and the South Sandwich Islands",
-            "Spain",
-            "Sri Lanka",
-            "Sudan",
-            "Suriname",
-            "Svalbard and Jan Mayen",
-            "Swaziland",
-            "Sweden",
-            "Switzerland",
-            "Syrian Arab Republic",
-            "Taiwan, Province of China",
-            "Tajikistan",
-            "Tanzania, United Republic of",
-            "Thailand",
-            "Timor-Leste",
-            "Togo",
-            "Tokelau",
-            "Tonga",
-            "Trinidad and Tobago",
-            "Tunisia",
-            "Turkey",
-            "Turkmenistan",
-            "Turks and Caicos Islands",
-            "Tuvalu",
-            "Uganda",
-            "Ukraine",
-            "United Arab Emirates",
-            "United Kingdom",
-            "United States",
-            "United States Minor Outlying Islands",
-            "Uruguay",
-            "Uzbekistan",
-            "Vanuatu",
-            "Venezuela",
-            "Viet Nam",
-            "Virgin Islands, British",
-            "Virgin Islands, US",
-            "Wallis and Futuna",
-            "Western Sahara",
-            "Yemen",
-            "Zambia",
-            "Zimbabwe"
-      };
+            int[] ArrayA = new int[] { 1, 2, 3, 4, 5, 6 };
+            int[] ArrayB = new int[6];
+            // copy by operator =
+            ArrayB = ArrayA;
 
-            // C# program that uses LINQ
-            var sortDescending = from var2 in array1
-                                 orderby var2 descending
-                                 select var2;
+            Console.WriteLine("*** Array copy by operator = ***");
+            Console.WriteLine("===== Before =====");
 
-            //var sortDescending = from var2 in array1
-            //                     orderby var2 descending
-            //                     select var2;
+            for (int i = 0; i < ArrayA.Length; i++)
+            {
+                Console.WriteLine("arrayA[{0}] = {1}, ArrayB[{0}] = {2}", i, ArrayA[i], ArrayB[i]);
+            }
+            // change element 0 of ArrayA
+            ArrayA[0] = 9;
+            Console.WriteLine("===== After ======");
+            for (int i = 0; i < ArrayA.Length; i++)
+            {
+                Console.WriteLine("ArrayA[{0}] = {1}, " +
+                    "ArrayB[{0}] = {2}", i, ArrayA[i], ArrayB[i]);
+            }
 
-            Console.WriteLine("------String sort descending----------");
-            foreach (string c in sortDescending)
-                Console.WriteLine(c);
 
+            // copy by method Array.Copy()
+            int[] ArrayC = new int[6];
+            Array.Copy(ArrayA, ArrayC, ArrayA.Length);
+
+            Console.WriteLine("*** Array copy by method Array.Copy() ***");
+            Console.WriteLine("===== Before =====");
+            for (int i = 0; i < ArrayA.Length; i++)
+            {
+                Console.WriteLine("ArrayA[{0}] = {1}, ArrayC[{0}] = {2}", i, ArrayA[i], ArrayC[i]);
+            }
+            // change element 0 of ArrayA
+            ArrayA[0] = 1;
+            Console.WriteLine("===== After =====");
+            for (int i = 0; i < ArrayA.Length; i++)
+            {
+                Console.WriteLine("ArrayA[{0}] = {1},ArrayC[{0}] = {2}", i, ArrayA[i], ArrayC[i]);
+            }
             // wait
             Console.ReadLine();
         }
