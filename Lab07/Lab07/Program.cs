c91e0c98b68cbef9b161e259ce3ed6353d1a96ac
pkmitl205 <58030205@kmitl.ac.th>
2017-11-13 12:15:04 +0700

    เรียงลำดับชื่อตามตัวอักษรจากน้อยไปมาก

--- Lab07/Lab07/Program.cs
+++ Lab07/Lab07/Program.cs
@@ -7,44 +7,19 @@ namespace ConsoleAppArray
     {
         static void Main(string[] args)
         {
-            // Array of characters.
-            char[] array1 = { 'q', 'w', 'e', 'r', 't',
-                'y', 'u', 'i', 'o', 'p' };
-            Array.Sort(array1);
-
-            // Array of strings.
-            string[] colors = new string[] { "red",
-              "orange", "blue", "green", "yellow", "lemon", "aqua"  };
-            Array.Sort(colors);
-
-            string[] AseanCountries = new string[] { "Cambodia", "Malaysia",
-              "Indonesia", "Singapore", "Thailand", "Philippines",
-              "Vietnam", "Brunei Darussalam", "Laos", "Myanmar" };
+            string[] array1 = { "January", "February", "March", "April", "May", "June",
+                                "July", "August", "September", "October", "November",
+                                "December"};
 
             // C# program that uses LINQ
-            var sortAscending = from country in AseanCountries
-                                orderby country
-                                select country;
-            var sortDescending = from country in AseanCountries
-                                 orderby country descending
-                                 select country;
-
-            // print output
-            Console.WriteLine("------Character sorting----------");
-            foreach (var c in array1)
-                Console.WriteLine(c);
-
-            Console.WriteLine("------String sorting----------");
-            foreach (string color in colors)
-                Console.WriteLine(color);
+            var sortAscending = from var1 in array1
+                                orderby var1
+                                select var1;
 
             Console.WriteLine("------String sort ascending----------");
             foreach (string c in sortAscending)
                 Console.WriteLine(c);
 
-            Console.WriteLine("------String sort descending----------");
-            foreach (string c in sortDescending)
-                Console.WriteLine(c);
             // wait
             Console.ReadLine();
         }
