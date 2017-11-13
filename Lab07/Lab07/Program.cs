7df869ec8ee16c766a9a32eeea375370a2663eca
pkmitl205 <58030205@kmitl.ac.th>
2017-11-14 00:58:52 +0700

    แบบฝึกหัด 1.สร้าง Jagged array โดยแถวที่ 1 บรรจุชื่อวันทั้ง 7 วัน และแถวที่ 2 บรรจุชื่อเดือนทั้ง 12 เดือน

--- Lab07/Lab07/Program.cs
+++ Lab07/Lab07/Program.cs
@@ -1,35 +1,54 @@
 ﻿using System;
-
-namespace ArrayOfArray
+namespace InterviewQuestionPart2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int[][] jagged = new int[7][];
-            int count = 0;
-            for (int row = 0; row < jagged.GetLength(0); ++row)
+            //Creations of jaggedArray    
+            string[][] jaggedArray = new string[2][];
+
+            //Array of string Array    
+            jaggedArray[0] = new string[7];
+            jaggedArray[1] = new string[12];
+ 
+            //values of 1st string Array = Day
+            jaggedArray[0][0] = "Sunday";
+            jaggedArray[0][1] = "Monday";
+            jaggedArray[0][2] = "Tuesday";
+            jaggedArray[0][3] = "Wednesday";
+            jaggedArray[0][4] = "Thursday";
+            jaggedArray[0][5] = "Friday";
+            jaggedArray[0][6] = "Saturday";
+    
+            //values of 2nd string Array = Month
+            jaggedArray[1][0] = "January";
+            jaggedArray[1][1] = "February";
+            jaggedArray[1][2] = "March";
+            jaggedArray[1][3] = "April";
+            jaggedArray[1][4] = "May";
+            jaggedArray[1][5] = "June";
+            jaggedArray[1][6] = "July";
+            jaggedArray[1][7] = "August";
+            jaggedArray[1][8] = "September";
+            jaggedArray[1][9] = "October";
+            jaggedArray[1][10] = "November";
+            jaggedArray[1][11] = "December";
+
+            //for getting string array in jagged array    
+            for (int i = 0; i < jaggedArray.Length; i++)
             {
-                Console.Write("\nRow {0}:", row);
-                jagged[row] = new int[row + 1];
-                for (int index = 0; index < row + 1; ++index)
+                string[] innerArray = jaggedArray[i];
+                //for getting values of innerArray or string Array    
+                for (int j = 0; j < innerArray.Length; j++)
                 {
-                    ++count;
-                    jagged[row][index] = count;
-                    Console.Write(" {0}", count);
+                    //print the values    
+                    Console.WriteLine(innerArray[j]);
                 }
+                //for blank space    
+                Console.WriteLine();
             }
-            Console.WriteLine("\n\nTotals");
-            for (int row = 0; row < jagged.GetLength(0); ++row)
-            {
-                int total = 0;
-                for (int index = 0; index < jagged[row].GetLength(0); ++index)
-                {
-                    total += jagged[row][index];
-                }
-                Console.Write("\nRow {0}: {1}", row, total);
-            }
-            Console.ReadLine();
+
         }
     }
 }
