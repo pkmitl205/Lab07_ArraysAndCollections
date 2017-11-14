9665f47e2ce2872605fed4bd4723ae1732f81d3f
pkmitl205 <58030205@kmitl.ac.th>
2017-11-14 21:07:35 +0700

    แบบฝึกหัด dictionary - 1

--- Lab07/Lab07/Program.cs
+++ Lab07/Lab07/Program.cs
@@ -8,32 +8,50 @@ namespace DictionaryDemo
     {
         static void Main(string[] args)
         {
-            Dictionary<string, string> country = new Dictionary<string, string>();
-            country.Add("AF", "Afghanistan");
-            country.Add("AL", "Albania");
-            country.Add("AS", "American Samoa");
-            country.Add("AD", "Andorra");
-            country.Add("AM", "Armenia");
-            country.Add("AW", "Aruba");
-            country.Add("AU", "Australia");
-            country.Add("AT", "Austria");
-            country.Add("AZ", "Azerbaijan");
-            foreach (var item in country)
-            {
-                Console.WriteLine("{0,2} = {1}", item.Key, item.Value);
-            }
-            Console.WriteLine("Count of items = {0}", country.Count);
-            Console.WriteLine("\n** Direct access to value by key **");
-            country["AU"] = "AUSTRALIA";
-            Console.WriteLine("AU = " + country["AU"]);
+            Dictionary<string, string> dict1 = new Dictionary<string, string>();
+            dict1.Add("txt", "Notepad.exe");
+            dict1.Add("bmp", "paint.exe");
+            dict1.Add("rtf", "wordpad.exe");
+            dict1.Add("pdf", "acrobat.exe");
 
-            Console.WriteLine("\n** Remove by key **");
-            country.Remove("AU");
-            foreach (var item in country)
+
+
+            Console.Write("Please Enter Text : ");
+
+            string var = Console.ReadLine();
+            if (var == "txt")
             {
-                Console.WriteLine("{0,2} = {1}", item.Key, item.Value);
+                if (dict1.ContainsKey("txt"))
+                {
+                    string value = dict1["txt"];
+                    Console.WriteLine(value);
+                }
             }
-            Console.WriteLine("Count of items = {0}", country.Count);
+            if (var == "bmp")
+            {
+                if (dict1.ContainsKey("bmp"))
+                {
+                    string value = dict1["bmp"];
+                    Console.WriteLine(value);
+                }
+            }
+            if (var == "rtf")
+            {
+                if (dict1.ContainsKey("rtf"))
+                {
+                    string value = dict1["rtf"];
+                    Console.WriteLine(value);
+                }
+            }
+            if (var == "pdf")
+            {
+                if (dict1.ContainsKey("pdf"))
+                {
+                    string value = dict1["pdf"];
+                    Console.WriteLine(value);
+                }
+            }
+
             Console.ReadLine();
         }
     }
