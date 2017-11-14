7a37949ae8d1fa31741b486a766778ade997abe7
pkmitl205 <58030205@kmitl.ac.th>
2017-11-15 00:43:30 +0700

    คำถาม 2. แฮชเทเบิล ที่บรรจุรหัสนักศึกษาและรายชื่อเพื่อนๆ ในห้องเรียน  (ทำในแขนงคอมพิวเตอร์เท่านั้น)

--- Lab07/Lab07/Program.cs
+++ Lab07/Lab07/Program.cs
@@ -8,25 +8,36 @@ class Program
     {
         Hashtable hashtable = new Hashtable();
 
-        hashtable.Add("10520", "Krung Thep Maha Nakhon");
-        hashtable.Add("81000", "Krabi");
-        hashtable.Add("71000", "Kanchanaburi");
-        hashtable.Add("46000", "Kalasin"); 
-        hashtable.Add("62000", "Kamphaeng Phet");
-        hashtable.Add("40000", "Khon Kaen");
-        hashtable.Add("22000", "Chanthaburi");
-        hashtable.Add("24000", "Chachoengsao");
-        hashtable.Add("20000", "Chon Buri");
-        hashtable.Add("20240", "Chai Nat");
-        hashtable.Add("36000", "Chaiyaphum");
-        hashtable.Add("86000", "Chumphon");
-        hashtable.Add("57000", "Chiang Rai");
-        hashtable.Add("50000", "Chiang Mai");
-        hashtable.Add("92000", "Trang");
-        hashtable.Add("23000", "Trat");
-        hashtable.Add("63000", "Tak");
-
-
+        hashtable.Add("58030142", "นายจักรดาว วณะทอง");
+        hashtable.Add("58030146", "นายจิรวัฒน์  กาแก้ว");
+        hashtable.Add("58030147", "นายจิรศักดิ์  เสน่หา");
+        hashtable.Add("58030151", "นางสาวฉัตรวรินทร์  จันทร์เหลือง");
+        hashtable.Add("58030163", "นางสาวณพร  โชคโภคาสมบัติ");
+        hashtable.Add("58030166", "นางสาวณัฐวรรณ  ขันธุปัทม์");
+        hashtable.Add("58030169", "นายดนัย  ทับอ่ำ");
+        hashtable.Add("58030171", "นายธนกฤต  จันทรานิมิตร");
+        hashtable.Add("58030174", "นายธนพล  สุทธิชื่น");
+        hashtable.Add("58030177", "นายธนากร  แสงทอง");
+        hashtable.Add("58030184", "นายธนากร  แสงทอง");
+        hashtable.Add("58030189", "นายปารเมศ  ขุมทอง"); 
+        hashtable.Add("58030190", "นางสาวปิยธิดา  แพงหอม");
+        hashtable.Add("58030196", "นางสาวพรศิริ  หอมเนียม");
+        hashtable.Add("58030198", "นายพัฒน์พงค์  แจ่มใส");
+        hashtable.Add("58030205", "นายภากร  รุจิเรขเสรีกุล");
+        hashtable.Add("58030206", "นายภาณุกิตติ์  แสงสว่าง");
+        hashtable.Add("58030207", "นายภาณุพงศ์  ธรรมชาติ");
+        hashtable.Add("58030209", "นายภานุพงศ์  สะและหมัด");
+        hashtable.Add("58030217", "นายวรปรัชญ์  นิเกศรี");
+        hashtable.Add("58030218", "นางสาววรรณนิษา  พรหมบุตร");
+        hashtable.Add("58030219", "นางสาววราภรณ์  ภูมิมาโนช");
+        hashtable.Add("58030228", "นายวุฒิพงษ์  ฆ้องพรมมา");
+        hashtable.Add("58030229", "นายศรัญญู  ตำลิ่ม");
+        hashtable.Add("58030231", "นายศุภชัย  ศรีพรรณา");
+        hashtable.Add("58030239", "นางสาวสุทธิดา  ตราชู");
+        hashtable.Add("58030240", "นางสาวสุทธิพร  สายทอง");
+        hashtable.Add("58030241", "นางสาวสุภนิดา  ซื่อตรงกูล");
+        hashtable.Add("58030245", "นายอดิเทพ  อินทรพล");
+        hashtable.Add("58030249", "นางสาวอุไรพร  ขีโรท");
 
         return hashtable;
     }
@@ -40,94 +51,159 @@ class Program
 
         string val = Console.ReadLine();
         
-        if (val == "10520")
+        if (val == "58030142")
         {
-        string value1 = (string)hashtable["10520"];
+        string value1 = (string)hashtable["58030142"];
             Console.WriteLine(value1);
         }
-        if (val == "81000")
+        if (val == "58030146")
         {
-            string value1 = (string)hashtable["81000"];
+            string value1 = (string)hashtable["58030146"];
             Console.WriteLine(value1);
         }
-        if (val == "71000")
+        if (val == "58030147")
         {
-            string value1 = (string)hashtable["71000"];
+            string value1 = (string)hashtable["58030147"];
             Console.WriteLine(value1);
         }
-        if (val == "46000")
+        if (val == "58030151")
         {
-            string value1 = (string)hashtable["46000"];
+            string value1 = (string)hashtable["58030151"];
             Console.WriteLine(value1);
         }
-        if (val == "62000")
+        if (val == "58030163")
         {
-            string value1 = (string)hashtable["62000"];
+            string value1 = (string)hashtable["58030163"];
             Console.WriteLine(value1);
         }
-        if (val == "40000")
+        if (val == "58030166")
         {
-            string value1 = (string)hashtable["40000"];
+            string value1 = (string)hashtable["58030166"];
             Console.WriteLine(value1);
         }
-        if (val == "22000")
+        if (val == "58030169")
         {
-            string value1 = (string)hashtable["22000"];
+            string value1 = (string)hashtable["58030169"];
             Console.WriteLine(value1);
         }
-        if (val == "24000")
+        if (val == "58030171")
         {
-            string value1 = (string)hashtable["24000"];
+            string value1 = (string)hashtable["58030171"];
             Console.WriteLine(value1);
         }
-        if (val == "20000")
+        if (val == "58030174")
         {
-            string value1 = (string)hashtable["20000"];
+            string value1 = (string)hashtable["58030174"];
             Console.WriteLine(value1);
         }
-        if (val == "20240")
+        if (val == "58030177")
         {
-            string value1 = (string)hashtable["20240"];
+            string value1 = (string)hashtable["58030177"];
             Console.WriteLine(value1);
         }
-        if (val == "36000")
+        if (val == "58030184")
         {
-            string value1 = (string)hashtable["36000"];
+            string value1 = (string)hashtable["58030184"];
             Console.WriteLine(value1);
         }
-        if (val == "86000")
+        if (val == "58030189")
         {
-            string value1 = (string)hashtable["86000"];
+            string value1 = (string)hashtable["58030189"];
             Console.WriteLine(value1);
         }
-        if (val == "57000")
+        if (val == "58030190")
         {
-            string value1 = (string)hashtable["57000"];
+            string value1 = (string)hashtable["58030190"];
             Console.WriteLine(value1);
         }
-        if (val == "50000")
+        if (val == "58030196")
         {
-            string value1 = (string)hashtable["50000"];
+            string value1 = (string)hashtable["58030196"];
             Console.WriteLine(value1);
         }
-        if (val == "92000")
+        if (val == "58030198")
         {
-            string value1 = (string)hashtable["92000"];
+            string value1 = (string)hashtable["58030198"];
             Console.WriteLine(value1);
         }
-        if (val == "23000")
+        if (val == "58030205")
         {
-            string value1 = (string)hashtable["23000"];
+            string value1 = (string)hashtable["58030205"];
             Console.WriteLine(value1);
         }
-        if (val == "63000")
+        if (val == "58030206")
         {
-            string value1 = (string)hashtable["63000"];
+            string value1 = (string)hashtable["58030206"];
             Console.WriteLine(value1);
         }
-        if (val == "26000")
+        if (val == "58030206")
         {
-            string value1 = (string)hashtable["26000"];
+            string value1 = (string)hashtable["58030206"];
+            Console.WriteLine(value1);
+        }
+        if (val == "58030207")
+        {
+            string value1 = (string)hashtable["58030207"];
+            Console.WriteLine(value1);
+        }
+        if (val == "58030209")
+        {
+            string value1 = (string)hashtable["58030209"];
+            Console.WriteLine(value1);
+        }
+        if (val == "58030217")
+        {
+            string value1 = (string)hashtable["58030217"];
+            Console.WriteLine(value1);
+        }
+        if (val == "58030218")
+        {
+            string value1 = (string)hashtable["58030218"];
+            Console.WriteLine(value1);
+        }
+        if (val == "58030219")
+        {
+            string value1 = (string)hashtable["58030219"];
+            Console.WriteLine(value1);
+        }
+        if (val == "58030228")
+        {
+            string value1 = (string)hashtable["58030228"];
+            Console.WriteLine(value1);
+        }
+        if (val == "58030229")
+        {
+            string value1 = (string)hashtable["58030229"];
+            Console.WriteLine(value1);
+        }
+        if (val == "58030231")
+        {
+            string value1 = (string)hashtable["58030231"];
+            Console.WriteLine(value1);
+        }
+        if (val == "58030239")
+        {
+            string value1 = (string)hashtable["58030239"];
+            Console.WriteLine(value1);
+        }
+        if (val == "58030240")
+        {
+            string value1 = (string)hashtable["58030240"];
+            Console.WriteLine(value1);
+        }
+        if (val == "58030241")
+        {
+            string value1 = (string)hashtable["58030241"];
+            Console.WriteLine(value1);
+        }
+        if (val == "58030245")
+        {
+            string value1 = (string)hashtable["58030245"];
+            Console.WriteLine(value1);
+        }
+        if (val == "58030249")
+        {
+            string value1 = (string)hashtable["58030249"];
             Console.WriteLine(value1);
         }
     }
