475b8f44f8840808822a923a33a3565f49b01450
pkmitl205 <58030205@kmitl.ac.th>
2017-11-14 21:18:08 +0700

    การใช้งานแฮชเทเบิลเบื้องต้น

--- Lab07/Lab07/Program.cs
+++ Lab07/Lab07/Program.cs
@@ -1,99 +1,24 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
 
-namespace DictionaryDemo
+namespace HashTableDemo
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Dictionary<string, string> country = new Dictionary<string, string>();
-            country.Add("AF", "Afghanistan");
-            country.Add("AL", "Albania");
-            country.Add("AS", "American Samoa");
-            country.Add("AD", "Andorra");
-            country.Add("AM", "Armenia");
-            country.Add("AW", "Aruba");
-            country.Add("AU", "Australia");
-            country.Add("AT", "Austria");
-            country.Add("AZ", "Azerbaijan");
+            Hashtable weeks = new Hashtable();
+            weeks.Add("1", "SunDay");
+            weeks.Add("2", "MonDay");
+            weeks.Add("3", "TuesDay");
+            weeks.Add("4", "WednesDay");
+            weeks.Add("5", "ThursDay");
+            weeks.Add("6", "FriDay");
+            weeks.Add("7", "SaturDay");
 
-
-            Console.Write("Please Enter Text : ");
-
-            string var = Console.ReadLine();
-            if (var == "AF")
+            foreach (DictionaryEntry day in weeks)
             {
-                if (country.ContainsKey("AF"))
-                {
-                    string value = country["AF"];
-                    Console.WriteLine(value);
-                }
-            }
-            if (var == "AL")
-            {
-                if (country.ContainsKey("AL"))
-                {
-                    string value = country["AL"];
-                    Console.WriteLine(value);
-                }
-            }
-            if (var == "AS")
-            {
-                if (country.ContainsKey("AS"))
-                {
-                    string value = country["AF"];
-                    Console.WriteLine(value);
-                }
-            }
-            if (var == "AD")
-            {
-                if (country.ContainsKey("AD"))
-                {
-                    string value = country["AD"];
-                    Console.WriteLine(value);
-                }
-            }
-            if (var == "AM")
-            {
-                if (country.ContainsKey("AM"))
-                {
-                    string value = country["AM"];
-                    Console.WriteLine(value);
-                }
-            }
-            if (var == "AW")
-            {
-                if (country.ContainsKey("AW"))
-                {
-                    string value = country["AW"];
-                    Console.WriteLine(value);
-                }
-            }
-            if (var == "AU")
-            {
-                if (country.ContainsKey("AU"))
-                {
-                    string value = country["AU"];
-                    Console.WriteLine(value);
-                }
-            }
-            if (var == "AT")
-            {
-                if (country.ContainsKey("AT"))
-                {
-                    string value = country["AT"];
-                    Console.WriteLine(value);
-                }
-            }
-            if (var == "AZ")
-            {
-                if (country.ContainsKey("AZ"))
-                {
-                    string value = country["AZ"];
-                    Console.WriteLine(value);
-                }
+                Console.WriteLine(day.Key + "   -   " + day.Value);
             }
             Console.ReadLine();
         }
