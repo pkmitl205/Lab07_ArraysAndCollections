3030d92a372dd4e0cb26f399e43495bda17623ad
pkmitl205 <58030205@kmitl.ac.th>
2017-11-14 14:49:51 +0700

    คิว (Queue)

--- Lab07/Lab07/Program.cs
+++ Lab07/Lab07/Program.cs
@@ -1,29 +1,48 @@
 ﻿using System;
-using System.Collections;
+using System.Collections.Generic;
 
-namespace ArrayListDemo
+namespace QueueDemo
 {
     class Program
     {
         static void Main(string[] args)
         {
-            ArrayList list1 = new ArrayList();
-            list1.Add(1);
-            list1.Add(2);
-            list1.Add(3);
-            list1.Add(4);
-            list1.Add(5);
-            Console.WriteLine("list1 count = {0}, capacity = {1}",
-list1.Count, list1.Capacity);
-            ArrayList list2 = new ArrayList(20);
-            Console.WriteLine("list2 count = {0}, capacity = {1}",
-list2.Count, list2.Capacity);
-            ArrayList list3 = new ArrayList(list1);
-            Console.WriteLine("list3 count = {0}, capacity = {1}",
-list3.Count, list3.Capacity);
-            ArrayList list4 = new ArrayList(list2);
-            Console.WriteLine("list4 count = {0}, capacity = {1}",
-list4.Count, list4.Capacity);
+            Queue<int> q = new Queue<int>();
+            Console.WriteLine("-- enqueue 3 items -- ");
+            q.Enqueue(10);
+            q.Enqueue(20);
+            q.Enqueue(30);
+            Console.Write("Queue contains : ");
+            foreach (var item in q)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine(Environment.NewLine + "q.Count  = {0}", q.Count);
+            Console.WriteLine("q.Peek = {0}", q.Peek());
+
+            Console.WriteLine("-- enqueue more items -- ");
+            q.Enqueue(40);
+            q.Enqueue(50);
+
+            Console.Write("Queue contains : ");
+            foreach (var item in q)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine(Environment.NewLine + "q.Count  = {0}", q.Count);
+            Console.WriteLine("q.Peek = {0}", q.Peek());
+
+            Console.WriteLine("-- dequeue -- ");
+            int i = q.Dequeue();
+            Console.WriteLine("i = {0}", i);
+
+            Console.Write("Queue contains : ");
+            foreach (var item in q)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine(Environment.NewLine + "q.Count  = {0}", q.Count);
+            Console.WriteLine("q.Peek = {0}", q.Peek());
             Console.ReadLine();
         }
     }
